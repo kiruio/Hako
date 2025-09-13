43733908f3fd09dd2717ddcbf2b44296a0fe55a8
BiliRumble <bilirumble@outlook.com>
2025-09-13 15:42:55 +0800

    Feat(UI): 自定义窗口装饰
    - [UI] 实现自定义窗口装饰
    - [UI] 移除内置字体, 使用系统字体
    - [Core] 修复`crash_handler`中的非法字符问题
    - [Deps] 调整`Iced`到位于`master`分支的`v0.14.0-dev`, 以支持`IME`
    - [Ver] Bump 0.0.1 -> 0.0.2

--- src/crash_handler.rs
+++ src/crash_handler.rs
@@ -13,12 +13,12 @@ pub fn hook() {
 
 fn generate_crash_report(info: &panic::PanicHookInfo) {
     const VERSION: &str = env!("CARGO_PKG_VERSION_FULL");
-    let formatted_time = Local::now().format("%Y_%m_%d-%H:%M:%S").to_string();
+    let formatted_time = Local::now().format("%Y_%m_%d %H:%M:%S").to_string();
     let filename = format!("crash_report_{}.log", formatted_time);
 
-    if let Ok(mut file) = File::create(&filename) {
+    if let Ok(mut file) = File::create(filename.replace(":", "_")) {
         let _ = writeln!(file, "Hako v{} Crash report", VERSION);
-        let _ = writeln!(file, "{}\n", formatted_time);
+        let _ = writeln!(file, "At {}\n", formatted_time);
 
         if let Some(s) = info.payload().downcast_ref::<&str>() {
             let _ = writeln!(file, "Cause: {}", s);
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use iced::{Font, Settings, Size, window};
+use iced::{Size, window};
 
 use crate::ui::root::Application;
 
@@ -12,19 +12,16 @@ fn main() {
     crash_handler::hook();
     env_logger::init();
 
-    let _ = iced::application("Hako", Application::update, Application::view)
-        .settings(Settings {
-            id: Some("Hako".to_string()),
-            fonts: vec![include_bytes!("../resources/fonts/NotoSansSC-Regular.ttf").into()],
-            default_font: Font::with_name("Noto Sans SC"),
-            antialiasing: true,
-            ..Default::default()
-        })
+    iced::application(Application::new, Application::update, Application::view)
+        .title("Hako")
         .window(window::Settings {
             size: Size::new(1050., 590.),
             min_size: Some(Size::new(1050., 590.)),
+            decorations: false,
             transparent: true,
             ..Default::default()
         })
-        .run();
+        .style(Application::style)
+        .run()
+        .expect("Could not run application");
 }
