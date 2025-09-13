4783e71b4a95a47fe05a36300a3f26814c5b0de7
BiliRumble <bilirumble@outlook.com>
2025-09-13 15:42:55 +0800

    Feat(UI): 自定义窗口装饰
    - [UI] 实现自定义窗口装饰
    - [UI] 移除内置字体, 使用系统字体
    - [Core] 修复`crash_handler`中的非法字符问题
    - [Deps] 调整`Iced`到位于`master`分支的`v0.14.0-dev`, 以支持`IME`
    - [Ver] Bump 0.0.1 -> 0.0.2

--- src/ui/root.rs
+++ src/ui/root.rs
@@ -1,31 +1,144 @@
-use iced::Element;
-use iced::widget::{button, column, text};
+use iced::{
+    Color, Element, Task, Theme,
+    theme::Style,
+    window::{self, Direction},
+};
 
 #[derive(Default, Debug)]
-pub struct Application {}
+pub struct Application {
+    content: String,
+}
 
 #[derive(Clone, Debug)]
 pub enum Message {
-    Crash,
+    Close,
+    Drag,
+    Minimize,
+    Resize(window::Direction),
+
+    ContentChanged(String),
 }
 
 impl Application {
-    pub fn update(&mut self, _message: Message) {
-        match _message {
-            Message::Crash => panic!("Crash!"),
+    pub fn new() -> (Self, Task<Message>) {
+        (Self::default(), Task::none())
+    }
+
+    pub fn update(&mut self, message: Message) -> Task<Message> {
+        match message {
+            Message::Close => window::latest().and_then(window::close),
+            Message::Drag => window::latest().and_then(window::drag),
+            Message::Minimize => window::latest().and_then(|id| window::minimize(id, true)),
+            Message::Resize(direction) => {
+                window::latest().and_then(move |id| window::drag_resize(id, direction))
+            }
+
+            Message::ContentChanged(content) => {
+                self.content = content;
+                Task::none()
+            }
         }
     }
 
     pub fn view(&self) -> Element<'_, Message> {
+        use iced::{
+            Background, Length, Padding, border,
+            mouse::Interaction,
+            widget::{Space, button, column, container, mouse_area, row, text, text_input},
+        };
+
+        let navbar = row![
+            text("Hako").size(24),
+            Space::with_width(Length::Fill),
+            row![button("启动"), button("下载"), button("设置"),].height(40),
+            Space::with_width(Length::Fill),
+            row![
+                button("_").on_press(Message::Minimize),
+                button("×").on_press(Message::Close),
+            ]
+        ]
+        .align_y(iced::Alignment::Center)
+        .padding(Padding::from([5, 12]))
+        .height(50);
+
+        let content = container(column![
+            mouse_area(navbar).on_press(Message::Drag),
+            column![
+                text("English"),
+                text("Deutsch"),
+                text("Français"),
+                text("Italiano"),
+                text("中文"),
+                text("日本語"),
+                text("한국어"),
+                text_input("Type something here...", &self.content)
+                    .on_input(Message::ContentChanged)
+                    .size(20)
+            ]
+            .height(Length::Fill)
+        ])
+        .style(|_theme: &Theme| container::Style {
+            background: Some(Background::Color(_theme.palette().background)),
+            border: border::Border {
+                width: 0.0,
+                radius: 8.0.into(),
+                color: Color::TRANSPARENT,
+            },
+            ..Default::default()
+        })
+        .clip(true)
+        .width(Length::Fill)
+        .height(Length::Fill);
+
+        let resize_handle = |direction| {
+            let interaction = match direction {
+                Direction::NorthWest | Direction::SouthEast => Interaction::ResizingDiagonallyDown,
+                Direction::NorthEast | Direction::SouthWest => Interaction::ResizingDiagonallyUp,
+                _ => Interaction::default(),
+            };
+            mouse_area(Space::with_width(Length::Fixed(4.)).height(Length::Fixed(4.)))
+                .interaction(interaction)
+                .on_press(Message::Resize(direction))
+        };
+
+        let resize_fill_handle = |direction| {
+            let (width, height) = match direction {
+                Direction::East | Direction::West => (Length::Fixed(4.0), Length::Fill),
+                _ => (Length::Fill, Length::Fixed(4.0)),
+            };
+            mouse_area(Space::with_width(width).height(height))
+                .interaction(match direction {
+                    Direction::North | Direction::South => Interaction::ResizingVertically,
+                    Direction::East | Direction::West => Interaction::ResizingHorizontally,
+                    _ => Interaction::default(),
+                })
+                .on_press(Message::Resize(direction))
+        };
+
         column![
-            text("Hello World!"),
-            text("你好世界！"),
-            text("こんにちは世界！"),
-            text("Español ¡Hola Mundo!"),
-            text("Здравствуй, мир!"),
-            text("🤤❗🤮🤗🎶😜💖"),
-            button("Don't touch me!").on_press(Message::Crash)
+            row![
+                resize_handle(Direction::NorthWest),
+                resize_fill_handle(Direction::North),
+                resize_handle(Direction::NorthEast),
+            ],
+            row![
+                resize_fill_handle(Direction::West),
+                content,
+                resize_fill_handle(Direction::East),
+            ],
+            row![
+                resize_handle(Direction::SouthWest),
+                resize_fill_handle(Direction::South),
+                resize_handle(Direction::SouthEast),
+            ],
         ]
         .into()
+    }
+
+    pub fn style(&self, theme: &Theme) -> Style {
+        Style {
+            background_color: Color::TRANSPARENT,
+            text_color: theme.palette().text,
+        }
     }
 }
