1a57a89654518e1343b91980b5e4d44eeb03c2dd
Yamrc <yamrci@outlook.com>
2025-10-19 22:53:22 +0800

    Feat(UI): 引入基础`Theme`框架
    - 实现明暗+主题色功能的`Theme`框架
    - 接入系统主题初始化与订阅，热重载主题

--- src/ui/views/settings.rs
+++ src/ui/views/settings.rs
@@ -1,9 +1,46 @@
-use iced::widget::{column, text};
-use iced::{Element, Length};
+use iced::Element;
+use iced::widget::{button, column, row, text, text_input};
 
-#[derive(Default, Debug, Clone)]
-pub struct State {}
+#[derive(Debug, Clone)]
+pub struct State {
+    pub primary_hex: String,
+}
 
-pub fn view<'a>(_state: &State) -> Element<'a, crate::ui::router::Message> {
-    column![text("Settings")].height(Length::Fill).into()
+#[derive(Clone, Debug)]
+pub enum Message {
+    PrimaryChanged(String),
+    ModeAuto,
+    ModeLight,
+    ModeDark,
+}
+
+// just for demo :)
+impl State {
+    pub fn update(&mut self, msg: &Message) {
+        if let Message::PrimaryChanged(hex) = msg {
+            self.primary_hex = hex.clone();
+        }
+    }
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self {
+            primary_hex: String::from("#5865F2"),
+        }
+    }
+}
+
+pub fn view<'a>(state: &State) -> Element<'a, Message> {
+    let input = text_input("Primary HEX", &state.primary_hex).on_input(Message::PrimaryChanged);
+    let controls = row![
+        button("Auto").on_press(Message::ModeAuto),
+        button("Light").on_press(Message::ModeLight),
+        button("Dark").on_press(Message::ModeDark),
+    ]
+    .spacing(8);
+
+    let content = column![text("Settings"), input, controls].spacing(12);
+
+    content.into()
 }
